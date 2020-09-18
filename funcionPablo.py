e2d2a8c9ae601315843483bd0be922cc41938b65
NicoleCG <nicole.carvajalg@gmail.com>
2020-09-17 21:23:15 -0400

    el mensaje python

---
+++ funcionPablo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 21:19:45 2020
+
+@author: Nicole
+"""
+
+def funcionPablo(lista):
+    salida = 0
+    for i in lista:
+        salida += i
+        
+    return salida
