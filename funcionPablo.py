4f5486fe8a811d3f69ca1d1fa42f81f0d28772eb
PabloZV <pablo.zuniga@ug.uchile.cl>
2020-09-17 22:33:48 -0300

    :tada: :tada: :tada: se agregan comentarios a funciones y verificación de tipos

--- funcionPablo.py
+++ funcionPablo.py
@@ -6,11 +6,14 @@ Created on Thu Sep 17 21:19:45 2020
 """
 
 def funcionPablo(lista):
+    #esta funcion retorna la suma de elementos de una lista
+    assert type(lista)==list
     salida = 0
     for i in lista:
         salida += i
     return salida
 
 def funcionNicole(palabra):
-    
+    #esta funcion retorna la palabra invertida
+    assert type(palabra)==str
     return palabra[::-1]
